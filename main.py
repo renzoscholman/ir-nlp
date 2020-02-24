62b33e2cecf1b3c91c106165e582d800a9d95589
Paulvanderlaan <P.J.vanderlaan-1@student.tudelft.nl>
2020-02-24 14:04:21 +0100

    fix tuple extraction

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ DATA_PATH = './data/url-versions-2015-06-14-clean.csv'
 def extract_article_headers(data_path, headers):
 	with open(data_path, mode='r') as csv_file:
 		csv_reader = csv.DictReader(csv_file)
-		rows = []
+		rows = [tuple(headers)]
 		for row in csv_reader:
-			row_data = [(header, row[header]) for header in headers]
-			rows.append(row_data)
+			row_data = [(row[header]) for header in headers]
+			rows.append(tuple(row_data))
 
 		return rows
 
 data = extract_article_headers(DATA_PATH, ['articleHeadline', 'articleHeadlineStance'])
-print(rows[0])
+print(f'Headers: {data[0]}')
+print(f'First row: {data[1]}')
