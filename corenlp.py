2480ef43564725f113c87499ebaa3bdaa56a9f44
Renzo Scholman <renzoscholman@gmail.com>
2020-02-29 18:59:11 +0100

    Added comment about which versions of Stanford CoreNLP are used.

--- corenlp.py
+++ corenlp.py
@@ -24,6 +24,8 @@ def get_rootdist_matrix(data, column):
 
 class NLPRootDist:
     # Can be initiated with other models location and other treebanks (en_ewt, en_gum, en_lines)
+    # Tested with Stanford CoreNLP models for English: en_ewt, en_gum and en_lines, all version 0.2.0.
+    # Default model is en_ewt
     def __init__(self, model_location='./corenlp_models', treebank='en_ewt'):
         self.nlp = stanfordnlp.Pipeline(lang="en", treebank=treebank, models_dir=model_location)
         self.hedging_words = [
